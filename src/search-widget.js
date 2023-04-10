9a3bbdb65020a5bd7a4ed0445a55dab0fd77b2f7
Zach <113333499+ZachGTheG@users.noreply.github.com>
2023-04-09 22:09:14 -0400

    search bar added

--- src/search-widget.js
+++ src/search-widget.js
@@ -1,44 +1,46 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html, css } from 'lit';
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
 export class SearchWidget extends LitElement {
-  static properties = {
-    value: {type: String},
-  };
+  static get properties() {
+    return {
+      value: { type: String },
+    }
+  }
 
   static get styles() {
     return css`
-      :host{
-        display: block; 
-      }
-      simple-icon{
-        display: inline block;
-        --simple-icon-height: 40px;
-        --simple-icon-width: 40px;
-      }
+    :host {
+      display: block;
+    }
+    simple-icon {
+      display: inline-block;
+      --simple-icon-height: 40px;
+      --simple-icon-width: 40px;
+    }
     `;
   }
 
   constructor() {
     super();
-    this.value = " ";
+    this.value = 'Default Value';
   }
 
   render() {
     return html`
-      <simple-icon icon="icons-search"></simple-icons>
-      <input type="text" value="${this.value}" @input="${this._handleInput}">
+      <simple-icon icon="icons:search"></simple-icon>
+      <input type="text" value="${this.value}" @input="${this._handleInput}" />
     `;
   }
-  
-  _handleInput(e){
-    this.value = e.targetValue;
-    console.log(this.value);
-    this.dispatchEvent(new CustomerEvent('search', {
-        detail: this.value,
+  _handleInput(e) {
+    this.value = e.target.value;
+    this.dispatchEvent(new CustomEvent('value-changed', {
+      detail: {
+        value: this.value,
+      }
     }));
   }
 }
 
-customElements.define("search-widget", SearchWidget);
+customElements.define('search-widget', SearchWidget);
